bed2fbb0843e2598abbe9e1473a34bf91c2b5513
Mondayrris <56475489+mondayrris@users.noreply.github.com>
2022-04-07 01:47:27 +0800

    created a test file and call it on default controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,14 @@
 
 namespace App\Controllers;
 
+use Myapp\Settings\Greet;
+
 class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $h = new Greet();
+        echo $h->hello();
+//        return view('welcome_message');
     }
 }
---
+++ app/ThirdParty/myapp-settings/src/Greet.php
@@ -0,0 +1,9 @@
+<?php namespace Myapp\Settings;
+
+class Greet
+{
+    public function hello()
+    {
+        return 'Hey, there!';
+    }
+}
